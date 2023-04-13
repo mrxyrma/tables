40c106f002e63c26e3a7beac514b2709268c185b
Andrey Korolev <ander_kor@mail.ru>
2023-04-13 22:34:18 +0300

    Сделал динамическое заполнение фильтров

--- src/components/PowerSupplies/Filters/Filters.js
+++ src/components/PowerSupplies/Filters/Filters.js
@@ -4,120 +4,33 @@ import './Filters.css'
 
 function Filters() {
   
-  const filtersTitles = ['Бренд', 
-              'Количество фаз на входе', 
-              'Номинальное выходное напряжение Uн, В DC',
-              'Номинальная мощность Pн, Вт',
-              'Номинальный ток Iн, А',
-              'Тип монтажа',
-              'Форм-фактор',
-              'Регулировка напряжения',
-              'Наличие контакта DC OK'];
+  const filtersTitles = Object.keys(data.blocks[0]).filter(item => !(item === 'id' || item === 'Артикул' || item === 'Наименование'));
 
-  const brand = Array.from(new Set(data.blocks.map(item => item.brand))).sort().map(item => {
-    return(<label className='filter__label'>
-      <input type='checkbox' value={item} className='filter__checkbox' /> {item}
-    </label>
+  const filters = filtersTitles.map(fieldsetTitle => {
+    const values = Array.from(new Set(data.blocks.map(item => item[`${fieldsetTitle}`])))
+      .sort((a, b) => a - b)
+      .map(label => {
+        return(
+          <label className='filter__label'>
+            <input type='checkbox' value={label} className='filter__checkbox' /> 
+            {label}
+          </label>
+        )
+      }
+    );
+
+    return(
+      <fieldset className='fieldset'>
+        <legend className='filter__title'>{fieldsetTitle}</legend>
+          {values}
+      </fieldset>
     )
-  });
+  }); 
 
-  const phase = Array.from(new Set(data.blocks.map(item => item.phase))).sort().map(item => {
-    return(<label className='filter__label'>
-      <input type='checkbox' value={item} className='filter__checkbox'/> {item}
-    </label>
-    )
-  });
-
-  const voltage = Array.from(new Set(data.blocks.map(item => item.voltage))).sort((a, b) => a - b).map(item => {
-    return(<label className='filter__label'>
-      <input type='checkbox' value={item} className='filter__checkbox'/> {item}
-    </label>
-    )
-  });
-
-  const power = Array.from(new Set(data.blocks.map(item => item.power))).sort((a, b) => a - b).map(item => {
-    return(<label className='filter__label'>
-      <input type='checkbox' value={item} className='filter__checkbox'/> {item}
-    </label>
-    )
-  });
-
-  const current = Array.from(new Set(data.blocks.map(item => item.current))).sort((a, b) => a - b).map(item => {
-    return(<label className='filter__label'>
-      <input type='checkbox' value={item} className='filter__checkbox'/> {item}
-    </label>
-    )
-  });
-
-  const type = Array.from(new Set(data.blocks.map(item => item.type))).sort().map(item => {
-    return(<label className='filter__label'>
-      <input type='checkbox' value={item} className='filter__checkbox'/> {item}
-    </label>
-    )
-  });
-
-  const formFactor = Array.from(new Set(data.blocks.map(item => item.formFactor))).sort().map(item => {
-    return(<label className='filter__label'>
-      <input type='checkbox' value={item} className='filter__checkbox'/> {item}
-    </label>
-    )
-  });
-
-  const voltageAdj = Array.from(new Set(data.blocks.map(item => item.voltageAdj))).sort().map(item => {
-    return(<label className='filter__label'>
-      <input type='checkbox' value={item} className='filter__checkbox'/> {item}
-    </label>
-    )
-  });
-
-  const dcOk = Array.from(new Set(data.blocks.map(item => item.dcOk))).sort().map(item => {
-    return(<label className='filter__label'>
-      <input type='checkbox' value={item} className='filter__checkbox'/> {item}
-    </label>
-    )
-  });
-
-  
- 
-     return(
+    return(
       <>
         <form className='filter'>
-          <fieldset className='fieldset'>
-          <legend className='filter__title'>{filtersTitles[0]}</legend>
-            {brand}
-          </fieldset>
-          <fieldset className='fieldset'>
-          <legend className='filter__title'>{filtersTitles[1]}</legend>
-            {phase}
-          </fieldset>
-          <fieldset className='fieldset'>
-          <legend className='filter__title'>{filtersTitles[2]}</legend>
-            {voltage}
-          </fieldset>
-          <fieldset className='fieldset'>
-          <legend className='filter__title'>{filtersTitles[3]}</legend>
-            {power}
-          </fieldset>
-          <fieldset className='fieldset'>
-          <legend className='filter__title'>{filtersTitles[4]}</legend>
-            {current}
-          </fieldset>
-          <fieldset className='fieldset'>
-          <legend className='filter__title'>{filtersTitles[5]}</legend>
-            {type}
-          </fieldset>
-          <fieldset className='fieldset'>
-          <legend className='filter__title'>{filtersTitles[6]}</legend>
-            {formFactor}
-          </fieldset>
-          <fieldset className='fieldset'>
-          <legend className='filter__title'>{filtersTitles[7]}</legend>
-            {voltageAdj}
-          </fieldset>
-          <fieldset className='fieldset'>
-          <legend className='filter__title'>{filtersTitles[8]}</legend>
-            {dcOk}
-          </fieldset>
+          {filters}
         </form>
       </>
     )
--- src/components/PowerSupplies/Table/Item/Item.js
+++ src/components/PowerSupplies/Table/Item/Item.js
@@ -1,21 +1,12 @@
 import './Item.css'
 
 function Item(props) {
-   const {brand, vendorCode, name, phase, voltage,power, current, type, formFactor, voltageAdj, dcOk} = props;
+   
     return(
         <li className='list-item'>
-            <p className='list-item__cell'>{brand}</p>
-            <p className='list-item__cell'>{vendorCode}</p>
-            <p className='list-item__cell'>{name}</p>
-            {/* <p className='list-item__cell'>{phase}</p> */}
-            {/* <p className='list-item__cell'>{voltage}</p>
-            <p className='list-item__cell'>{power}</p>
-            <p className='list-item__cell'>{current}</p> */}
-            {/* <p className='list-item__cell'>{type}</p>
-            <p className='list-item__cell'>{formFactor}</p> */}
-            {/* <p className='list-item__cell'>{voltageAdj}</p>
-            <p className='list-item__cell'>{dcOk}</p> */}
-
+            <p className='list-item__cell'>{props['Бренд']}</p>
+            <p className='list-item__cell'>{props['Артикул']}</p>
+            <p className='list-item__cell'>{props['Наименование']}</p>
         </li>
     )
 }
