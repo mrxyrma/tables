619d7a56a9972c8dbb2deb12c090c76e3f5952a0
Andrey Korolev <ander_kor@mail.ru>
2023-04-14 22:24:33 +0300

    Реализовал передачу выбранных фильтров в стейт

--- src/components/PowerSupplies/PowerSupplies.js
+++ src/components/PowerSupplies/PowerSupplies.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Filters from './Filters/Filters';
 import Table from './Table/Table';
 
@@ -5,12 +7,24 @@ import './PowerSupplies.css';
 
 function PowerSupplies() {
 
- return(
+  const [selectedFilters, setSelectedFilters] = useState('');
+  
+  function selected() {
+    const fieldests = document.querySelectorAll('.fieldset');
+    
+    setSelectedFilters(Array.from(fieldests).map(fieldset => {
+      return Array.from(fieldset.elements).filter(elem => elem.checked).map(item => item.value);
+    })
+    )
+  }
+
+  return(
     <>
       <h1 className='title'>Подбор источников питания</h1>
       <div className="table-page__content">
         <aside className='filters'>
           <h2 className='subtitle'>Фильтры:</h2>
+          <button onClick={selected}>Click me!</button>
           <Filters />
         </aside>
         <Table />
