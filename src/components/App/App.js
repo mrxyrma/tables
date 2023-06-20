3eaeb6a77f4c8bb9ee9d43cfec4f377b4cf7016c
Andrey <ander_kor@mail.ru>
2023-06-20 12:14:55 +0300

    Добавил таблицу с автоматами защиты двигателя

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import powerSupplies from '../../sources/power-supplies';
 import dinRails from '../../sources/din-rails';
+import mpsb from '../../sources/mpsb';
 
 import Header from '../Header/Header';
 import TablesCards from '../TablesCards/TablesCards';
@@ -20,6 +21,7 @@ function App() {
           <Route path='/' element={<TablesCards />}/>
           <Route path='/power-supplies' element={<SelectionPage data={powerSupplies}/>}/>
           <Route path='/din-rails' element={<SelectionPage data={dinRails}/>}/>
+          <Route path='/mpsb' element={<SelectionPage data={mpsb}/>}/>
         </Routes>
       </main>
       
--- src/components/TablesCards/TablesCards.js
+++ src/components/TablesCards/TablesCards.js
@@ -20,9 +20,9 @@ function TablesCards() {
           </Link>
         </li>
         <li className="card__item">
-          <Link className='card__item_link' to='/power-supplies'>
-            <img className='card__item_image' src='https://files.keaz.ru/f/38757/obschaya_category.jpg'/>
-            <p className='card__item_name'>Источники питания</p>
+          <Link className='card__item_link' to='/mpsb'>
+            <img className='card__item_image' src='https://uploads.chint.com/uploads/product_user/image/file/ac7aec48-c44a-44f4-bc58-2a701993b08e.jpg'/>
+            <p className='card__item_name'>Автоматы защиты двигателя</p>
           </Link>
         </li>
         <li className="card__item">
