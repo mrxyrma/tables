ad49db2e4730ab774fc1b1a7322f88da7a555580
Andrey Korolev <ander_kor@mail.ru>
2023-07-11 22:27:24 +0300

    Стилизовал и адаптировал страницу отдельного изделия

--- client/src/components/ProductPage/ProductPage.js
+++ client/src/components/ProductPage/ProductPage.js
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 
 import './ProductPage.css'
 import { useEffect, useState } from 'react';
+
 function ProductPage() {
   const {tableId, productId} = useParams();
   const [product, setProduct] = useState([{}]);
@@ -11,23 +12,45 @@ function ProductPage() {
       .then(res => res.json())
       .then(data => {
         setProduct(data)
-        console.log(data)
       })
   }, [])
 
+ const params = () => {
+  const paramsArray = []
+
+  for (let key in product[0]) {
+    if (key !== '_id' && key !== 'Артикул' && key !== 'Наименование')
+    paramsArray.push(`${key}: ${product[0][key]}`)
+  }
+
+  const view = paramsArray.map(item => {
+    return(
+      <p className='product__param'>{item}</p>
+    )
+  })
+
+  return view
+}
+
   return(
-    <div>
-      <img 
+    <div className='product'>
+      <div className='product__info'>
+        <img
+          className='product__image'
           src="https://files.keaz.ru/f/38757/obschaya_category.jpg"
           alt="product"
-          className='product__image'/>
-      <div className='product__title'>
-        <h2>{product[0]['Артикул']} </h2>
-        <h2>{product[0]['Наименование']}</h2>
+          />
+        <div>
+          <h2 className='product__name'>{product[0]['Наименование']} </h2>
+          <h3 className='product__order-number'>{product[0]['Артикул']}</h3>
+          <p className='product__subtitle'>Технические характеристики:</p>
+          {params()}
+        </div>
       </div>
-      
-      <p>Аксессуары:</p>
-      <p>Тут будет раздел с аксессуарами</p> 
+      <div>
+        <p className='product__subtitle'>Принадлежности:</p>
+        <p>Тут будет раздел с аксессуарами</p>
+      </div>
     </div>
   )
 }
