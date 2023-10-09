4da2315ca5c44546e201cf86b22516932cbc4d54
Andrey Korolev <ander_kor@mail.ru>
2023-10-09 23:15:37 +0300

    Переписал логику работы филдсетов для работы с пустыми ячейками таблицы

--- client/src/components/SelectionPage/Filters/Fieldset/Fieldset.js
+++ client/src/components/SelectionPage/Filters/Fieldset/Fieldset.js
@@ -1,39 +1,56 @@
-import FilterButton from './FilterButton/FilterButton';
+import FilterButton from './FilterButton/FilterButton'
 
-import './Fieldset.css';
+import './Fieldset.css'
 
-function Fieldset({title, values, getSelectedBtns}) {
+function Fieldset({ title, values, getSelectedBtns }) {
+  const pureArr = values.filter((item) => typeof item !== 'undefined') //Массив значений без незаполненных ячеек таблицы
   let sortedValues
-  if (values.every(item => typeof(item) === 'string')) {
-    sortedValues = values.sort().map(label => <FilterButton key={label} label={label}/>)
+  if (pureArr.every((item) => typeof item === 'string')) {
+    sortedValues = pureArr.sort().map((label) => (
+      <FilterButton
+        key={label}
+        label={label}
+      />
+    ))
   } else {
-    sortedValues = values.sort((a, b) => a - b).map(label => <FilterButton key={label} label={label}/>)
+    sortedValues = pureArr
+      .sort((a, b) => a - b)
+      .map((label) => (
+        <FilterButton
+          key={label}
+          label={label}
+        />
+      ))
   }
-  
-  function takeSelectedBtns(e){
+
+  function takeSelectedBtns(e) {
     const makeCorrectFormat = (item) => {
       let correctFormatValue = Number(item.value)
-      if(String(correctFormatValue) === 'NaN') {
-        correctFormatValue = item.value
-      }
+      if (correctFormatValue === 0) return undefined
+      if (String(correctFormatValue) === 'NaN') correctFormatValue = item.value
       return correctFormatValue
     }
 
-    let selectedBtns = Array.from(e.currentTarget.elements).filter(item => item.checked).map(makeCorrectFormat)
+    let selectedBtns = Array.from(e.currentTarget.elements)
+      .filter((item) => item.checked)
+      .map(makeCorrectFormat)
 
-    if(!selectedBtns.length) {
-      selectedBtns = Array.from(e.currentTarget.elements).map(makeCorrectFormat)
+    if (!selectedBtns.length) {
+      selectedBtns = values
     }
 
     const objectWithFieldset = {}
     objectWithFieldset[title] = selectedBtns
     getSelectedBtns(objectWithFieldset)
   }
-  
-  return(
-    <fieldset className='fieldset' onChange={takeSelectedBtns}>
+
+  return (
+    <fieldset
+      className='fieldset'
+      onChange={takeSelectedBtns}
+    >
       <legend className='fieldset__title'>{title}</legend>
-        {sortedValues}
+      {sortedValues}
     </fieldset>
   )
 }
--- client/src/components/SelectionPage/Filters/Filters.js
+++ client/src/components/SelectionPage/Filters/Filters.js
@@ -1,16 +1,34 @@
-import { memo } from 'react';
-import Fieldset from './Fieldset/Fieldset';
+import { memo } from 'react'
+import Fieldset from './Fieldset/Fieldset'
 
 import './Filters.css'
 
-function Filters({data, setItems, setInputValue}) {
-  const ignoreTitles = ['_id', 'Артикул', 'Наименование', 'src', 'Серия', 'Примечание'] //Список полей таблицы, которые игнорируются в филдсетах
-  const fieldsetTitles = Object.keys(data[0]).filter(item => !ignoreTitles.includes(item)) //Массив с заголовками филдсетов (шапка таблицы)
+function Filters({ data, setItems, setInputValue }) {
+  const ignoreTitles = [
+    '_id',
+    'Артикул',
+    'Наименование',
+    'src',
+    'Серия',
+    'Примечание',
+  ] //Список полей таблицы, которые игнорируются в филдсетах
 
-  let arrayWithFilters = [] //Каждый элемент - это объект, где ключ - название филдсета, а значение - массив с уникальными значениями
+  const allColumnTitles = new Set() //Все без исключения названия столбцов таблицы
+
+  data.map((item) =>
+    Object.keys(item).forEach((item) => allColumnTitles.add(item))
+  )
+
+  const fieldsetTitles = Array.from(allColumnTitles).filter(
+    (item) => !ignoreTitles.includes(item)
+  ) //Массив с заголовками филдсетов (шапка таблицы, но за исключением тех столбцов, которые не нужны)
+
+  let arrayWithFilters = [] //Актуальное значение фильтров всей формы. Каждый элемент - это объект, где ключ - название филдсета, а значение - массив с уникальными значениями
   fieldsetTitles.forEach((fieldsetTitle, index) => {
     const obj = {}
-    obj[fieldsetTitle] = Array.from(new Set(data.map(item => item[`${fieldsetTitle}`]))) //Объект с филдсетами в формате {Бренд: [КЭАЗ, Lovato...]} 
+    obj[fieldsetTitle] = Array.from(
+      new Set(data.map((item) => item[`${fieldsetTitle}`]))
+    ) //Объект с филдсетами в формате {Бренд: [КЭАЗ, Lovato...]}
     arrayWithFilters[index] = obj
   })
 
@@ -19,43 +37,54 @@ function Filters({data, setItems, setInputValue}) {
     arrayWithFilters[index] = objectWithFieldset
   }
 
-  const fieldsets = fieldsetTitles.map((fieldsetTitle, index) =>
-      <Fieldset
-        key={fieldsetTitle}
-        title={fieldsetTitle}
-        values={arrayWithFilters[index][fieldsetTitle]}
-        getSelectedBtns={getSelectedBtns}
-      />
-  )
+  const fieldsets = fieldsetTitles.map((fieldsetTitle, index) => (
+    <Fieldset
+      key={fieldsetTitle}
+      title={fieldsetTitle}
+      values={arrayWithFilters[index][fieldsetTitle]}
+      getSelectedBtns={getSelectedBtns}
+    />
+  ))
 
   function settingItems(e) {
     e.preventDefault()
 
-    const filteredItems = data.filter(item => {
-    
-      const arrWithValues = [] //массив со значеними каждой отдельной позиции из таблицы
+    const filteredItems = data.filter((item) => {
+      const arrWithValues = [] //массив со значениями каждой отдельной позиции из таблицы
 
-      fieldsetTitles.forEach(fieldsetTitle => {
+      fieldsetTitles.forEach((fieldsetTitle) => {
         arrWithValues.push(item[fieldsetTitle])
       })
 
-      return arrWithValues.every((arrItem, index) => arrayWithFilters[index][Object.keys(arrayWithFilters[index])[0]].includes(arrItem))
-
+      return arrWithValues.every((arrItem, index) =>
+        arrayWithFilters[index][
+          Object.keys(arrayWithFilters[index])[0]
+        ].includes(arrItem)
+      )
     })
+
     setInputValue('')
     setItems(filteredItems)
-    document.querySelector('.table').scrollIntoView({behavior: 'smooth', block: 'start'})
+
+    document
+      .querySelector('.table')
+      .scrollIntoView({ behavior: 'smooth', block: 'start' })
   }
-  
-  return(
+
+  return (
     <aside className='filters'>
-        <h2 className='subtitle'>Фильтры:</h2>
-        <form className='filter'>
-          {fieldsets}
-          <button className='filters__button' onClick={settingItems}>Подобрать!</button>
-        </form>
+      <h2 className='subtitle'>Фильтры:</h2>
+      <form className='filter'>
+        {fieldsets}
+        <button
+          className='filters__button'
+          onClick={settingItems}
+        >
+          Подобрать!
+        </button>
+      </form>
     </aside>
   )
 }
 
-export default memo(Filters);
+export default memo(Filters)
