48c13c3a43c0cc0e748497afe3518ccd3dfafcad
Andrey <ander_kor@mail.ru>
2023-12-12 10:13:36 +0300

    Изменил почту в футере с рабочей на личную

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,12 +6,11 @@ function Footer() {
       <div className='container'>
         <div className='footer__elements'>
           <div>
-            Сайт предназначен исключительно для внутреннего использования
-            сотрудниками компании
+            Сайт предназначен исключительно для внутреннего использования сотрудниками компании
           </div>
           <a
             className='footer__copy'
-            href='mailto:a.korolev@cety-telekom.ru?subject=Вопрос по сайту&body=Добрый день!'
+            href='mailto:ander_kor@mail.ru?subject=Вопрос по сайту&body=Добрый день!'
           >
             © 2023. Андрей Королёв
           </a>
